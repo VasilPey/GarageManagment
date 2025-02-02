8b52bcf55041308172aefd0a4957d65f71e25aac
VasilPeychinov <vaskobe1997@gmail.com>
2025-02-02 21:54:43 +0200

    Added unit tests

---
+++ GarageManagment/Tests/CarServiceTest.cs
@@ -0,0 +1,83 @@
+﻿using GarageManagment.Models;
+using GarageManagment.Repositories;
+using GarageManagment.Services.Impl;
+using Moq;
+using Xunit;
+
+namespace GarageManagment.Tests
+{
+    public class CarServiceTests
+    {
+        private readonly Mock<ICarRepository> _mockCarRepository;
+        private readonly CarServiceImpl _carService;
+
+        public CarServiceTests()
+        {
+            _mockCarRepository = new Mock<ICarRepository>();
+            _carService = new CarServiceImpl(_mockCarRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnListOfCars()
+        {
+            
+            var cars = new List<Car>
+            {
+                new Car { id = 1, Brand = "Toyota", Model = "Corolla", Color = "Red", YearOfProduction = 2020, HorsePower = 150 },
+                new Car { id = 2, Brand = "Honda", Model = "Civic", Color = "Blue", YearOfProduction = 2019, HorsePower = 140 }
+            };
+
+            _mockCarRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(cars);
+
+           
+            var result = await _carService.getAll();
+
+            
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+        }
+
+        [Fact]
+        public async Task GetCarById_ShouldReturnCar()
+        {
+            
+            var car = new Car { id = 1, Brand = "Toyota", Model = "Corolla", Color = "Red", YearOfProduction = 2020, HorsePower = 150 };
+            _mockCarRepository.Setup(repo => repo.GetById(1)).ReturnsAsync(car);
+
+            
+            var result = await _carService.getCarById(1);
+
+            
+            Assert.NotNull(result);
+            Assert.Equal(1, result.id);
+            Assert.Equal("Toyota", result.Brand);
+        }
+
+        [Fact]
+        public void AddCar_ShouldCallRepositoryAdd()
+        {
+          
+            var car = new Car { id = 1, Brand = "Toyota", Model = "Corolla", Color = "Red", YearOfProduction = 2020, HorsePower = 150 };
+
+        
+            _carService.addCar(car);
+
+           
+            _mockCarRepository.Verify(repo => repo.Add(It.IsAny<Car>()), Times.Once);
+            _mockCarRepository.Verify(repo => repo.Save(), Times.Once);
+        }
+
+        [Fact]
+        public void UpdateCar_ShouldCallRepositoryUpdate()
+        {
+           
+            var car = new Car { id = 1, Brand = "Toyota", Model = "Corolla", Color = "Red", YearOfProduction = 2020, HorsePower = 150 };
+
+            
+            _carService.updateCar(1, car);
+
+            
+            _mockCarRepository.Verify(repo => repo.Update(1, It.IsAny<Car>()), Times.Once);
+        }
+    }
+}
---
+++ GarageManagment/Tests/GarageServiceTests.cs
@@ -0,0 +1,84 @@
+﻿using GarageManagment.Models;
+using GarageManagment.Repositories;
+using GarageManagment.Services.Impl;
+using Moq;
+using Xunit;
+
+namespace GarageManagment.Tests
+{
+    public class GarageServiceTests
+    {
+        private readonly Mock<IGarageRepository> _mockGarageRepository;
+        private readonly Mock<ICarRepository> _mockCarRepository;
+        private readonly GarageServiceImpl _garageService;
+
+        public GarageServiceTests()
+        {
+            _mockGarageRepository = new Mock<IGarageRepository>();
+            _mockCarRepository = new Mock<ICarRepository>();
+            _garageService = new GarageServiceImpl(_mockGarageRepository.Object, _mockCarRepository.Object);
+        }
+
+        [Fact]
+        public async Task GetAll_ShouldReturnListOfGarages()
+        {
+            
+            var garages = new List<Garage>
+            {
+                new Garage { Id = 1, Name = "Garage A", Location = "Location A", Cars = new List<Car>() },
+                new Garage { Id = 2, Name = "Garage B", Location = "Location B", Cars = new List<Car>() }
+            };
+
+            _mockGarageRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(garages);
+
+           
+            var result = await _garageService.getAll();
+
+            
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+        }
+
+        [Fact]
+        public async Task GetGarageById_ShouldReturnGarage()
+        {
+            
+            var garage = new Garage { Id = 1, Name = "Garage A", Location = "Location A", Cars = new List<Car>() };
+            _mockGarageRepository.Setup(repo => repo.GetById(1)).ReturnsAsync(garage);
+
+            
+            var result = await _garageService.getGarageById(1);
+
+           
+            Assert.NotNull(result);
+            Assert.Equal(1, result.Id);
+            Assert.Equal("Garage A", result.Name);
+        }
+
+        [Fact]
+        public void AddGarage_ShouldCallRepositoryAddGarage()
+        {
+            
+            var garage = new Garage { Id = 1, Name = "Garage A", Location = "Location A", Cars = new List<Car>() };
+
+           
+            _garageService.addGarage(garage);
+
+           
+            _mockGarageRepository.Verify(repo => repo.AddGarage(It.IsAny<Garage>()), Times.Once);
+        }
+
+        [Fact]
+        public void DeleteGarage_ShouldCallRepositoryDelete()
+        {
+            
+            int garageId = 1;
+
+            
+            _garageService.deleteGarage(garageId);
+
+            
+            _mockGarageRepository.Verify(repo => repo.Delete(garageId), Times.Once);
+        }
+    }
+}
